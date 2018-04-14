0bc67016c9ce20096cf4ae2a387d414e51d6b50f
Efrain Perez Jr <efrainperezjr@live.com>
2018-04-14 23:33:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,4 +35,8 @@ class Trip {
   passenger() {
     return store.passengers.find((passenger) => passenger.id === this.passengerId);
   }
+
+  driver() {
+    return store.drivers.find((driver) => driver.id === this.driverId);
+  }
 }
