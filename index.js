abe8e20458d182b7cea43d15e192dc4a92885f76
Efrain Perez Jr <efrainperezjr@live.com>
2018-04-14 23:50:57 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,58 @@
+const store = {
+  drivers: [],
+  passengers: [],
+  trips: []
+};
+
+let driverIdCounter = 0;
+let passengerIdCounter = 0;
+let tripIdCounter = 0;
+
+class Driver {
+  constructor(name) {
+    this.id = ++driverIdCounter;
+    this.name = name;
+    store.drivers.push(this);
+  }
+
+  trips() {
+    return store.trips.filter((trip) => trip.driverId == this.id);
+  }
+
+  passengers() {
+    return this.trips().map((trip) => store.passengers.find((passenger) => passenger.id === trip.passengerId))
+  }
+}
+
+class Passenger {
+  constructor(name) {
+    this.id = ++passengerIdCounter;
+    this.name = name;
+    store.passengers.push(this);
+  }
+
+  trips() {
+    return store.trips.filter((trip) => trip.passengerId == this.id);
+  }
+
+  drivers() {
+    return this.trips().map((trip) => store.drivers.find((driver) => driver.id === trip.driverId));
+  }
+}
+
+class Trip {
+  constructor(driver, passenger) {
+    this.id = ++tripIdCounter;
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
+    store.trips.push(this);
+  }
+
+  passenger() {
+    return store.passengers.find((passenger) => passenger.id === this.passengerId);
+  }
+
+  driver() {
+    return store.drivers.find((driver) => driver.id === this.driverId);
+  }
+}
