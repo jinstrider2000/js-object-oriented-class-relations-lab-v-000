a803029ac613f1b80ca9413026ee08121702e4d7
Efrain Perez Jr <efrainperezjr@live.com>
2018-04-14 23:28:35 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,38 @@
+const store = {
+  drivers: [],
+  passengers: [],
+  trips: []
+};
+
+let driverIdCounter = 0;
+let passengerIdCounter = 0;
+let tripIdCounter = 0;
+
+class Driver {
+  constructor(name) {
+    this.id = ++driverIdCounter;
+    this.name = name;
+    store.drivers.push(this);
+  }
+}
+
+class Passenger {
+  constructor(name) {
+    this.id = ++passengerIdCounter;
+    this.name = name;
+    store.passengers.push(this);
+  }
+}
+
+class Trip {
+  constructor(driver, passenger) {
+    this.id = ++tripIdCounter;
+    this.driverId = driver.id;
+    this.passengerId = passenger.id;
+    store.trips.push(this);
+  }
+
+  passenger() {
+    return store.passengers.find((passenger) => passenger.id === this.passengerId);
+  }
+}
